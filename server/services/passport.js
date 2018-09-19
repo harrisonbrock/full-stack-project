e4018653e2ddcf14d0e81b33ce2dec9c57175606
Harrison Brock <harrison@harrisonbrock.com>
2018-09-19 16:21:47 -0400

    Fixed oauth to only added one user

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,12 +1,23 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 passport.use(new GoogleStrategy({
 	clientID: keys.googleClientID,
 	clientSecret: keys.googleSecret,
 	callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-	console.log(accessToken, refreshToken, profile, done);
+  User.findOne({googleId: profile.id})
+	.then((exitingUser) => {
+	  if (exitingUser) {
+
+	  }
+	  else {
+	    new User({googleId: profile.id}).save();
+	  }
+	});
+
   })
 );
