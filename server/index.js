7036647b20cfc15455234699a71891a62fa27960
Harrison Brock <harrison@harrisonbrock.com>
2018-09-19 15:41:00 -0400

    ADDED: Setup for mongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURL);
 const app = express();
 
 require('./routes/authRoutes')(app);
