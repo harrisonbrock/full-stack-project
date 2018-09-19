597aaa98a526662d023ddcd348f15af411bb0876
Harrison Brock <harrison@harrisonbrock.com>
2018-09-19 12:19:00 -0400

    ADDED: Route for the start of logging into Google

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ passport.use(new GoogleStrategy({
   })
 );
 
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+  })
+);
+
 app.get('/', (req, res) => {
   res.send({hi: 'there'})
 });
